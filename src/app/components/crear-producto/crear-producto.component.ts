c331158031711328bee11bf0a6702715dd7596be
Richard <ricki2099@hotmail.com>
2024-09-16 16:54:24 -0500

    feat: Se agrega validaciones y un formulario reactivo para crear productos

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-crear-producto',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './crear-producto.component.html',
-  styleUrl: './crear-producto.component.scss'
+  styleUrl: './crear-producto.component.scss',
 })
 export class CrearProductoComponent {
+  crearProductoForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.crearProductoForm = this.formBuilder.group({
+      producto: ['', Validators.required],
+      categoria: ['', Validators.required],
+      ubicacion: ['', Validators.required],
+      precio: ['', Validators.required],
+    });
+  }
+
+  agregarProducto() {
+    console.log(this.crearProductoForm);
+  }
 }
