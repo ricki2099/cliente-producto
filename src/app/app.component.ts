35ad5d725c23e134a6463919c90e0625a80a2125
Ricardo Rodriguez Bareno <rirodriguez@personalsoft.com>
2024-09-16 11:49:23 -0500

    fix: creacion de productos con angular, node, express, mongodb

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CrearProductoComponent,ListarProductosComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'cliente';
+}
